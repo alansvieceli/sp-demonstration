542f2502ba6a9c48eecde5b0f097252c45262fd6
Alan Vieceli <alansvieceli@gmail.com>
2024-04-08 20:06:54 -0300

    initial commit, base spring boot

---
+++ src/main/java/br/com/alan/Demonstration/SpringBootDemonstrationApplication.java
@@ -0,0 +1,13 @@
+package br.com.alan.Demonstration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemonstrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemonstrationApplication.class, args);
+	}
+
+}
